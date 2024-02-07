2dc9101d244f04a9da02daeefd71d3fe1a5d28d9
Fox <kostya.moroz.96@gmail.com>
2024-02-07 22:37:27 +0200

    fixes

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link, useLocation} from "react-router-dom";
 import logo from './logo.svg'
 import search from './search.svg'
@@ -8,6 +8,8 @@ import './header.scss'
 
 export default function Header() {
    let location = useLocation();
+   const [active, setActive] = useState(false)
+
    // console.log(location)
 
     return <div className={location.pathname === '/' ? "header main" : "header"}>
@@ -17,11 +19,12 @@ export default function Header() {
                     <img src={logo} alt=""/>
                 </Link>
             </div>
-            <div className="header__column header__column--2">
+            <div className={active ? "header__column header__column--2 open" :"header__column header__column--2"}>
                 <nav className="header__nav">
                     <Link className={location.pathname.includes('shop') ? 'active' : ''} to={'/shop'}>Shop</Link>
                     <Link className={location.pathname.includes('blog') ? 'active' : ''} to={'/blog'}>Blog</Link>
                     <Link className={location.pathname.includes('about-us') ? 'active' : ''} to={'/about-us'}>Our story</Link>
+                    <Link className={location.pathname.includes('contact') ? 'active mobile' : 'mobile'} to={'/contact'}>Contact</Link>
                 </nav>
                 <div className="header__icons">
                     <div className="header__icon">
@@ -31,8 +34,16 @@ export default function Header() {
                         <img src={cart} alt=""/>
                     </Link>
                     <Link className="header__icon" to={'/account'}>
-                        <img src={account} alt=""/>
+                        <img src={account} alt=""/> My account
                     </Link>
+                </div>
+            </div>
+            <div className="header__mobile-wrapper">
+                <Link to="/cart" className="header__icon">
+                    <img src={cart} alt=""/>
+                </Link>
+                <div className={active ? "header__mobile-button-wrapper open" :"header__mobile-button-wrapper"} onClick={() => setActive(prevState => !prevState)}>
+                    <div className="header__mobile-button"></div>
                 </div>
             </div>
         </div>
